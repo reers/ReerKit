ec9e6f1306cf209f0935ebc66ac4118ce7c39422
Phoenix <x.rhythm@qq.com>
2023-04-04 22:32:39 +0800

    feat: add NSPredicate extensions.

---
+++ Sources/Foundation/NSPredicate+REExtensions.swift
@@ -0,0 +1,91 @@
+//
+//  Copyright © 2020 SwifterSwift
+//  Copyright © 2022 reers.
+//
+//  Permission is hereby granted, free of charge, to any person obtaining a copy
+//  of this software and associated documentation files (the "Software"), to deal
+//  in the Software without restriction, including without limitation the rights
+//  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//  copies of the Software, and to permit persons to whom the Software is
+//  furnished to do so, subject to the following conditions:
+//
+//  The above copyright notice and this permission notice shall be included in
+//  all copies or substantial portions of the Software.
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+//  THE SOFTWARE.
+
+#if canImport(Foundation)
+import Foundation
+
+public extension Reer where Base: NSPredicate {
+    /// ReerKit Returns a new predicate formed by NOT-ing the predicate.
+    var not: NSCompoundPredicate {
+        return NSCompoundPredicate(notPredicateWithSubpredicate: base)
+    }
+
+    /// ReerKit Returns a new predicate formed by AND-ing the argument to the predicate.
+    ///
+    /// - Parameter predicate: NSPredicate.
+    /// - Returns: NSCompoundPredicate.
+    func and(_ predicate: NSPredicate) -> NSCompoundPredicate {
+        return NSCompoundPredicate(andPredicateWithSubpredicates: [base, predicate])
+    }
+
+    /// ReerKit Returns a new predicate formed by OR-ing the argument to the predicate.
+    ///
+    /// - Parameter predicate: NSPredicate.
+    /// - Returns: NSCompoundPredicate.
+    func or(_ predicate: NSPredicate) -> NSCompoundPredicate {
+        return NSCompoundPredicate(orPredicateWithSubpredicates: [base, predicate])
+    }
+}
+
+
+// MARK: - Operators
+
+public extension NSPredicate {
+    /// ReerKit Returns a new predicate formed by NOT-ing the predicate.
+    /// - Parameters: rhs: NSPredicate to convert.
+    /// - Returns: NSCompoundPredicate
+    static prefix func ! (rhs: NSPredicate) -> NSCompoundPredicate {
+        return rhs.re.not
+    }
+
+    /// ReerKit Returns a new predicate formed by AND-ing the argument to the predicate.
+    ///
+    /// - Parameters:
+    ///   - lhs: NSPredicate.
+    ///   - rhs: NSPredicate.
+    /// - Returns: NSCompoundPredicate
+    static func + (lhs: NSPredicate, rhs: NSPredicate) -> NSCompoundPredicate {
+        return lhs.re.and(rhs)
+    }
+
+    /// ReerKit Returns a new predicate formed by OR-ing the argument to the predicate.
+    ///
+    /// - Parameters:
+    ///   - lhs: NSPredicate.
+    ///   - rhs: NSPredicate.
+    /// - Returns: NSCompoundPredicate
+    static func | (lhs: NSPredicate, rhs: NSPredicate) -> NSCompoundPredicate {
+        return lhs.re.or(rhs)
+    }
+
+    /// ReerKit Returns a new predicate formed by remove the argument to the predicate.
+    ///
+    /// - Parameters:
+    ///   - lhs: NSPredicate.
+    ///   - rhs: NSPredicate.
+    /// - Returns: NSCompoundPredicate
+    static func - (lhs: NSPredicate, rhs: NSPredicate) -> NSCompoundPredicate {
+        return lhs + !rhs
+    }
+}
+
+#endif
---
+++ Tests/FoundationTests/NSPredicateExtensionsTests.swift
@@ -0,0 +1,120 @@
+//
+//  NSPredicateExtensionsTests.swift
+//  ReerKit-iOSTests
+//
+//  Created by phoenix on 2023/4/4.
+//  Copyright © 2023 reers. All rights reserved.
+//
+
+@testable import ReerKit
+import XCTest
+
+#if canImport(Foundation)
+import Foundation
+
+final class NSPredicateExtensionsTests: XCTestCase {
+    func testNot() {
+        let predicate = NSPredicate(value: true)
+        let notPredicate = predicate.re.not
+        XCTAssertEqual(notPredicate.compoundPredicateType, .not)
+
+        #if os(Linux)
+        XCTAssertEqual(notPredicate.subpredicates, [predicate])
+        #else
+        if let subpredicates = notPredicate.subpredicates as? [NSPredicate] {
+            XCTAssertEqual(subpredicates, [predicate])
+        }
+        #endif
+    }
+
+    func testAndPredicate() {
+        let predicate1 = NSPredicate(value: true)
+        let predicate2 = NSPredicate(value: false)
+        let andPredicate = predicate1.re.and(predicate2)
+        XCTAssertEqual(andPredicate.compoundPredicateType, .and)
+
+        #if os(Linux)
+        XCTAssertEqual(andPredicate.subpredicates, [predicate1, predicate2])
+        #else
+        if let subpredicates = andPredicate.subpredicates as? [NSPredicate] {
+            XCTAssertEqual(subpredicates, [predicate1, predicate2])
+        }
+        #endif
+    }
+
+    func testOrPredicate() {
+        let predicate1 = NSPredicate(value: true)
+        let predicate2 = NSPredicate(value: false)
+        let orPredicate = predicate1.re.or(predicate2)
+        XCTAssertEqual(orPredicate.compoundPredicateType, .or)
+
+        #if os(Linux)
+        XCTAssertEqual(orPredicate.subpredicates, [predicate1, predicate2])
+        #else
+        if let subpredicates = orPredicate.subpredicates as? [NSPredicate] {
+            XCTAssertEqual(subpredicates, [predicate1, predicate2])
+        }
+        #endif
+    }
+
+    func testOperatorNot() {
+        let predicate = NSPredicate(value: false)
+        let notPredicate = !predicate
+        XCTAssertEqual(notPredicate.compoundPredicateType, .not)
+
+        #if os(Linux)
+        XCTAssertEqual(notPredicate.subpredicates, [predicate])
+        #else
+        if let subpredicates = notPredicate.subpredicates as? [NSPredicate] {
+            XCTAssertEqual(subpredicates, [predicate])
+        }
+        #endif
+    }
+
+    func testOperatorAndPredicate() {
+        let predicate1 = NSPredicate(value: false)
+        let predicate2 = NSPredicate(value: true)
+        let andPredicate = predicate1 + predicate2
+        XCTAssertEqual(andPredicate.compoundPredicateType, .and)
+
+        #if os(Linux)
+        XCTAssertEqual(andPredicate.subpredicates, [predicate1, predicate2])
+        #else
+        if let subpredicates = andPredicate.subpredicates as? [NSPredicate] {
+            XCTAssertEqual(subpredicates, [predicate1, predicate2])
+        }
+        #endif
+    }
+
+    func testOperatorOrPredicate() {
+        let predicate1 = NSPredicate(value: true)
+        let predicate2 = NSPredicate(value: false)
+        let orPredicate = predicate1 | predicate2
+        XCTAssertEqual(orPredicate.compoundPredicateType, .or)
+
+        #if os(Linux)
+        XCTAssertEqual(orPredicate.subpredicates, [predicate1, predicate2])
+        #else
+        if let subpredicates = orPredicate.subpredicates as? [NSPredicate] {
+            XCTAssertEqual(subpredicates, [predicate1, predicate2])
+        }
+        #endif
+    }
+
+    func testOperatorSubPredicate() {
+        let predicate1 = NSPredicate(block: { value, _ in
+            guard let number = value as? Int else { return false }
+            return 1..<5 ~= number
+        })
+        let predicate2 = NSPredicate(block: { value, _ in
+            guard let number = value as? Int else { return false }
+            return 3..<6 ~= number
+        })
+
+        let subPredicate = predicate1 - predicate2
+        XCTAssert(subPredicate.evaluate(with: 2))
+        XCTAssertFalse(subPredicate.evaluate(with: 4))
+    }
+}
+
+#endif

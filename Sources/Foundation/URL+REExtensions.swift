1ebeacf919d3767c55c54a3521b35726193a8a12
Phoenix <x.rhythm@qq.com>
2023-04-05 01:35:17 +0800

    feat: add URL extensions

--- Sources/Foundation/URL+REExtensions.swift
+++ Sources/Foundation/URL+REExtensions.swift
@@ -1,4 +1,5 @@
 //
+//  Copyright © 2020 SwifterSwift
 //  Copyright © 2022 reers.
 //
 //  Permission is hereby granted, free of charge, to any person obtaining a copy
@@ -21,6 +22,141 @@
 
 #if canImport(Foundation)
 import Foundation
+
+#if canImport(UIKit) && canImport(AVFoundation)
+import AVFoundation
+import UIKit
+#endif
+
+public extension ReerForEquatable where Base == URL {
+    /// ReerKit: Dictionary of the URL's query parameters.
+    var queryParameters: [String: String]? {
+        guard let components = URLComponents(url: base, resolvingAgainstBaseURL: false),
+              let queryItems = components.queryItems
+        else { return nil }
+
+        var items: [String: String] = [:]
+
+        for queryItem in queryItems {
+            items[queryItem.name] = queryItem.value
+        }
+
+        return items
+    }
+
+    /// ReerKit: URL with appending query parameters.
+    ///
+    ///        let url = URL(string: "https://google.com")!
+    ///        let param = ["q": "Hello Swift"]
+    ///        url.appendingQueryParameters(params) -> "https://google.com?q=Hello%20Swift"
+    ///
+    /// - Parameter parameters: parameters dictionary.
+    /// - Returns: URL with appending given query parameters.
+    func appendingQueryParameters(_ parameters: [String: String]) -> URL {
+        var urlComponents = URLComponents(url: base, resolvingAgainstBaseURL: true)!
+        urlComponents.queryItems = (urlComponents.queryItems ?? []) + parameters
+            .map { URLQueryItem(name: $0, value: $1) }
+        return urlComponents.url!
+    }
+
+    /// ReerKit: Get value of a query key.
+    ///
+    ///    var url = URL(string: "https://google.com?code=12345")!
+    ///    queryValue(for: "code") -> "12345"
+    ///
+    /// - Parameter key: The key of a query value.
+    func queryValue(for key: String) -> String? {
+        return URLComponents(string: base.absoluteString)?
+            .queryItems?
+            .first(where: { $0.name == key })?
+            .value
+    }
+
+    /// ReerKit: Returns a new URL by removing all the path components.
+    ///
+    ///     let url = URL(string: "https://domain.com/path/other")!
+    ///     print(url.deletingAllPathComponents()) // prints "https://domain.com/"
+    ///
+    /// - Returns: URL with all path components removed.
+    func deletingAllPathComponents() -> URL {
+        guard !base.pathComponents.isEmpty else { return base }
+
+        var url: URL = base
+        for _ in 0..<base.pathComponents.count - 1 {
+            url.deleteLastPathComponent()
+        }
+        return url
+    }
+
+    /// ReerKit: Generates new URL that does not have scheme.
+    ///
+    ///        let url = URL(string: "https://domain.com")!
+    ///        print(url.droppedScheme()) // prints "domain.com"
+    func droppedScheme() -> URL? {
+        if let scheme = base.scheme {
+            let droppedScheme = String(base.absoluteString.dropFirst(scheme.count + 3))
+            return URL(string: droppedScheme)
+        }
+
+        guard base.host != nil else { return base }
+
+        let droppedScheme = String(base.absoluteString.dropFirst(2))
+        return URL(string: droppedScheme)
+    }
+
+    #if os(iOS) || os(tvOS)
+    /// ReerKit: Generate a thumbnail image from given url. Returns nil if no thumbnail could be created. This function may take some time to complete. It's recommended to dispatch the call if the thumbnail is not generated from a local resource.
+    ///
+    ///     var url = URL(string: "https://video.golem.de/files/1/1/20637/wrkw0718-sd.mp4")!
+    ///     var thumbnail = url.thumbnail()
+    ///     thumbnail = url.thumbnail(fromTime: 5)
+    ///
+    ///     DisptachQueue.main.async {
+    ///         someImageView.image = url.thumbnail()
+    ///     }
+    ///
+    /// - Parameter time: Seconds into the video where the image should be generated.
+    /// - Returns: The UIImage result of the AVAssetImageGenerator
+    func thumbnail(fromTime time: Float64 = 0) -> UIImage? {
+        let imageGenerator = AVAssetImageGenerator(asset: AVAsset(url: base))
+        let time = CMTimeMakeWithSeconds(time, preferredTimescale: 1)
+        var actualTime = CMTimeMake(value: 0, timescale: 0)
+
+        guard let cgImage = try? imageGenerator.copyCGImage(at: time, actualTime: &actualTime) else {
+            return nil
+        }
+        return UIImage(cgImage: cgImage)
+    }
+    #endif
+}
+
+extension URL: ReerReferenceCompatible {}
+public extension ReerReference where Base == URL {
+    /// ReerKit: Append query parameters to URL.
+    ///
+    ///        var url = URL(string: "https://google.com")!
+    ///        let param = ["q": "Hello Swift"]
+    ///        url.appendQueryParameters(params)
+    ///        print(url) // prints "https://google.com?q=Hello%20Swift"
+    ///
+    /// - Parameter parameters: parameters dictionary.
+    mutating func appendQueryParameters(_ parameters: [String: String]) {
+        base.pointee = base.pointee.re.appendingQueryParameters(parameters)
+    }
+
+    /// ReerKit: Remove all the path components from the URL.
+    ///
+    ///        var url = URL(string: "https://domain.com/path/other")!
+    ///        url.deleteAllPathComponents()
+    ///        print(url) // prints "https://domain.com/"
+    mutating func deleteAllPathComponents() {
+        guard !base.pointee.pathComponents.isEmpty else { return }
+
+        for _ in 0..<base.pointee.pathComponents.count - 1 {
+            base.pointee.deleteLastPathComponent()
+        }
+    }
+}
 
 // MARK: - Initializer
 
--- Tests/FoundationTests/URLExtensionsTests.swift
+++ Tests/FoundationTests/URLExtensionsTests.swift
@@ -16,4 +16,108 @@ final class URLExtensionsTests: XCTestCase {
         XCTAssertNotNil(URL.re(string: "myapp://example.com/over/there?hello=你好#nose"))
     }
 
+    var url = URL(string: "https://www.google.com")!
+    let params = ["q": "swifter swift"]
+    let queryUrl = URL(string: "https://www.google.com?q=swifter%20swift")!
+
+    func testQueryParameters() {
+        let url = URL(string: "https://www.google.com?q=swifter%20swift&steve=jobs&empty")!
+        guard let parameters = url.re.queryParameters else {
+            XCTAssert(false)
+            return
+        }
+
+        XCTAssertEqual(parameters.count, 2)
+        XCTAssertEqual(parameters["q"], "swifter swift")
+        XCTAssertEqual(parameters["steve"], "jobs")
+        XCTAssertNil(parameters["empty"])
+    }
+
+    func testOptionalStringInitializer() {
+        XCTAssertNil(URL.re(string: nil, relativeTo: nil))
+        XCTAssertNil(URL.re(string: nil))
+
+        let baseURL = URL(string: "https://www.example.com")
+        XCTAssertNotNil(baseURL)
+        XCTAssertNil(URL.re(string: nil, relativeTo: baseURL))
+
+        let string = "/index.html"
+        let optionalString: String? = string
+        XCTAssertEqual(URL.re(string: optionalString, relativeTo: baseURL), URL.re(string: string, relativeTo: baseURL))
+        XCTAssertEqual(
+            URL.re(string: optionalString, relativeTo: baseURL)?.absoluteString,
+            "https://www.example.com/index.html")
+    }
+
+    func testAppendingQueryParameters() {
+        XCTAssertEqual(url.re.appendingQueryParameters(params), queryUrl)
+    }
+
+    func testAppendQueryParameters() {
+        url.re.appendQueryParameters(params)
+        XCTAssertEqual(url, queryUrl)
+    }
+
+    func testValueForQueryKey() {
+        let url = URL(string: "https://google.com?code=12345&empty")!
+
+        let codeResult = url.re.queryValue(for: "code")
+        let emtpyResult = url.re.queryValue(for: "empty")
+        let otherResult = url.re.queryValue(for: "other")
+
+        XCTAssertEqual(codeResult, "12345")
+        XCTAssertNil(emtpyResult)
+        XCTAssertNil(otherResult)
+    }
+
+    func testDeletingAllPathComponents() {
+        let url = URL(string: "https://domain.com/path/other/")!
+        let result = url.re.deletingAllPathComponents()
+        XCTAssertEqual(result.absoluteString, "https://domain.com/")
+
+        let pathlessURL = URL(string: "https://domain.com")!
+        let pathlessResult = pathlessURL.re.deletingAllPathComponents()
+        XCTAssertEqual(pathlessResult.absoluteString, "https://domain.com")
+    }
+
+    func testDeleteAllPathComponents() {
+        var url = URL(string: "https://domain.com/path/other/")!
+        url.re.deleteAllPathComponents()
+        XCTAssertEqual(url.absoluteString, "https://domain.com/")
+
+        var pathlessURL = URL(string: "https://domain.com")!
+        pathlessURL.re.deleteAllPathComponents()
+        XCTAssertEqual(pathlessURL.absoluteString, "https://domain.com")
+    }
+
+    #if os(iOS) || os(tvOS)
+    func testThumbnail() {
+        XCTAssertNil(url.re.thumbnail())
+
+        let videoUrl = Bundle(for: URLExtensionsTests.self)
+            .url(forResource: "big_buck_bunny_720p_1mb", withExtension: "mp4")!
+        XCTAssertNotNil(videoUrl.re.thumbnail())
+        XCTAssertNotNil(videoUrl.re.thumbnail(fromTime: 1))
+    }
+    #endif
+
+    func testDropScheme() {
+        let urls: [String: String?] = [
+            "https://domain.com/path/other/": "domain.com/path/other/",
+            "https://domain.com": "domain.com",
+            "http://domain.com": "domain.com",
+            "file://domain.com/image.jpeg": "domain.com/image.jpeg",
+            "://apple.com": "apple.com",
+            "//apple.com": "apple.com",
+            "apple.com": "apple.com",
+            "http://": nil,
+            "//": "//"
+        ]
+
+        urls.forEach { input, expected in
+            guard let url = URL(string: input) else { return XCTFail("Failed to initialize URL.") }
+            XCTAssertEqual(url.re.droppedScheme()?.absoluteString, expected, "input url: \(input)")
+        }
+    }
+
 }

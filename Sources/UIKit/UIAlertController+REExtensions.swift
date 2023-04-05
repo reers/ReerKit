f3a41c22d33331050850b3b149d5de423415bd3c
Phoenix <x.rhythm@qq.com>
2023-04-05 15:19:07 +0800

    feat: add UIAlertController extensions

---
+++ Sources/UIKit/UIAlertController+REExtensions.swift
@@ -0,0 +1,146 @@
+//
+//  Copyright © 2020 SwifterSwift
+//  Copyright © 2022 reers.
+//
+//  Permission is hereby granted, free of charge, to any person obtaining a copy
+//  of this software and associated documentation files (the "Software"), to deal
+//  in the Software without restriction, including without limitation the rights
+//  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//  copies of the Software, and to permit persons to whom the Software is
+//  furnished to do so, subject to the following conditions:
+//
+//  The above copyright notice and this permission notice shall be included in
+//  all copies or substantial portions of the Software.
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+//  THE SOFTWARE.
+
+#if canImport(UIKit) && !os(watchOS)
+import UIKit
+
+#if canImport(AudioToolbox)
+import AudioToolbox
+#endif
+
+public extension Reer where Base: UIAlertController {
+    /// ReerKit: Present alert view controller in the current view controller.
+    ///
+    /// - Parameters:
+    ///   - animated: set true to animate presentation of alert controller (default is true).
+    ///   - vibrate: set true to vibrate the device while presenting the alert (default is false).
+    ///   - completion: an optional completion handler to be called after presenting alert controller (default is nil).
+    @available(iOSApplicationExtension, unavailable)
+    func show(animated: Bool = true, vibrate: Bool = false, completion: (() -> Void)? = nil) {
+        #if targetEnvironment(macCatalyst)
+        let window = UIApplication.shared.windows.last
+        #else
+        let window = UIApplication.shared.keyWindow
+        #endif
+        window?.rootViewController?.present(base, animated: animated, completion: completion)
+        if vibrate {
+            #if canImport(AudioToolbox)
+            AudioServicesPlayAlertSound(kSystemSoundID_Vibrate)
+            #endif
+        }
+    }
+
+    /// ReerKit: Add an action to Alert.
+    ///
+    /// - Parameters:
+    ///   - title: action title.
+    ///   - style: action style (default is UIAlertActionStyle.default).
+    ///   - isEnabled: isEnabled status for action (default is true).
+    ///   - handler: optional action handler to be called when button is tapped (default is nil).
+    /// - Returns: action created by this method.
+    @discardableResult
+    func addAction(
+        title: String,
+        style: UIAlertAction.Style = .default,
+        isEnabled: Bool = true,
+        handler: ((UIAlertAction) -> Void)? = nil
+    ) -> UIAlertAction {
+        let action = UIAlertAction(title: title, style: style, handler: handler)
+        action.isEnabled = isEnabled
+        base.addAction(action)
+        return action
+    }
+
+    /// ReerKit: Add a text field to Alert.
+    ///
+    /// - Parameters:
+    ///   - text: text field text (default is nil).
+    ///   - placeholder: text field placeholder text (default is nil).
+    ///   - editingChangedTarget: an optional target for text field's editingChanged.
+    ///   - editingChangedSelector: an optional selector for text field's editingChanged.
+    func addTextField(
+        text: String? = nil,
+        placeholder: String? = nil,
+        editingChangedTarget: Any?,
+        editingChangedSelector: Selector?
+    ) {
+        base.addTextField { textField in
+            textField.text = text
+            textField.placeholder = placeholder
+            if let target = editingChangedTarget, let selector = editingChangedSelector {
+                textField.addTarget(target, action: selector, for: .editingChanged)
+            }
+        }
+    }
+}
+
+// MARK: - Initializers
+
+public extension UIAlertController {
+    /// ReerKit: Create new alert view controller with default OK action.
+    ///
+    /// - Parameters:
+    ///   - title: alert controller's title.
+    ///   - message: alert controller's message (default is nil).
+    ///   - defaultActionButtonTitle: default action button title (default is "OK").
+    ///   - tintColor: alert controller's tint color (default is nil).
+    static func re(
+        title: String,
+        message: String? = nil,
+        defaultActionButtonTitle: String = "OK",
+        tintColor: UIColor? = nil
+    ) -> UIAlertController {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let defaultAction = UIAlertAction(title: defaultActionButtonTitle, style: .default, handler: nil)
+        alert.addAction(defaultAction)
+        if let color = tintColor {
+            alert.view.tintColor = color
+        }
+        return alert
+    }
+
+    /// ReerKit: Create new error alert view controller from Error with default OK action.
+    ///
+    /// - Parameters:
+    ///   - title: alert controller's title (default is "Error").
+    ///   - error: error to set alert controller's message to it's localizedDescription.
+    ///   - defaultActionButtonTitle: default action button title (default is "OK").
+    ///   - preferredStyle: type of alert to display (default is .alert).
+    ///   - tintColor: alert controller's tint color (default is nil).
+    static func re(
+        title: String = "Error",
+        error: Error,
+        defaultActionButtonTitle: String = "OK",
+        preferredStyle: UIAlertController.Style = .alert,
+        tintColor: UIColor? = nil
+    ) -> UIAlertController {
+        let alert = UIAlertController(title: title, message: error.localizedDescription, preferredStyle: preferredStyle)
+        let defaultAction = UIAlertAction(title: defaultActionButtonTitle, style: .default, handler: nil)
+        alert.addAction(defaultAction)
+        if let color = tintColor {
+            alert.view.tintColor = color
+        }
+        return alert
+    }
+}
+
+#endif

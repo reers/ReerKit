ceb05163e07d3397f649dfd1e188fc72a274a667
Phoenix <x.rhythm@qq.com>
2023-04-19 00:35:08 +0800

    feat: add UIButton extensions.

---
+++ Sources/UIKit/UIButton+REExtensions.swift
@@ -0,0 +1,237 @@
+//
+//  Copyright © 2020 SwifterSwift
+//  Copyright © 2022 reers.
+//
+//  Permission is hereby granted, free of charge, to any person obtaining a copy
+//  of this software and associated documentation files (the "Software"), to deal
+//  in the Software without restriction, including without limitation the rights
+//  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//  copies of the Software, and to permit persons to whom the Software is
+//  furnished to do so, subject to the following conditions:
+//
+//  The above copyright notice and this permission notice shall be included in
+//  all copies or substantial portions of the Software.
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+//  THE SOFTWARE.
+
+#if canImport(UIKit) && !os(watchOS)
+import UIKit
+
+public extension Reer where Base: UIButton {
+    /// ReerKit: Image of disabled state for button; also inspectable from Storyboard.
+    var imageForDisabled: UIImage? {
+        get { return base.image(for: .disabled) }
+        set { base.setImage(newValue, for: .disabled) }
+    }
+
+    /// ReerKit: Image of highlighted state for button; also inspectable from Storyboard.
+    var imageForHighlighted: UIImage? {
+        get { return base.image(for: .highlighted) }
+        set { base.setImage(newValue, for: .highlighted) }
+    }
+
+    /// ReerKit: Image of normal state for button; also inspectable from Storyboard.
+    var imageForNormal: UIImage? {
+        get { return base.image(for: .normal) }
+        set { base.setImage(newValue, for: .normal) }
+    }
+
+    /// ReerKit: Image of selected state for button; also inspectable from Storyboard.
+    var imageForSelected: UIImage? {
+        get { return base.image(for: .selected) }
+        set { base.setImage(newValue, for: .selected) }
+    }
+
+    /// ReerKit: Image of focused state for button; also inspectable from Storyboard.
+    var imageForFocused: UIImage? {
+        get { return base.image(for: .focused) }
+        set { base.setImage(newValue, for: .focused) }
+    }
+    
+    /// ReerKit: Title color of disabled state for button; also inspectable from Storyboard.
+    var titleColorForDisabled: UIColor? {
+        get { return base.titleColor(for: .disabled) }
+        set { base.setTitleColor(newValue, for: .disabled) }
+    }
+
+    /// ReerKit: Title color of highlighted state for button; also inspectable from Storyboard.
+    var titleColorForHighlighted: UIColor? {
+        get { return base.titleColor(for: .highlighted) }
+        set { base.setTitleColor(newValue, for: .highlighted) }
+    }
+
+    /// ReerKit: Title color of normal state for button; also inspectable from Storyboard.
+    var titleColorForNormal: UIColor? {
+        get { return base.titleColor(for: .normal) }
+        set { base.setTitleColor(newValue, for: .normal) }
+    }
+
+    /// ReerKit: Title color of selected state for button; also inspectable from Storyboard.
+    var titleColorForSelected: UIColor? {
+        get { return base.titleColor(for: .selected) }
+        set { base.setTitleColor(newValue, for: .selected) }
+    }
+
+    /// ReerKit: Title color of focused state for button; also inspectable from Storyboard.
+    var titleColorForFocused: UIColor? {
+        get { return base.titleColor(for: .focused) }
+        set { base.setTitleColor(newValue, for: .focused) }
+    }
+    
+    /// ReerKit: Title of disabled state for button; also inspectable from Storyboard.
+    var titleForDisabled: String? {
+        get { return base.title(for: .disabled) }
+        set { base.setTitle(newValue, for: .disabled) }
+    }
+
+    /// ReerKit: Title of highlighted state for button; also inspectable from Storyboard.
+    var titleForHighlighted: String? {
+        get { return base.title(for: .highlighted) }
+        set { base.setTitle(newValue, for: .highlighted) }
+    }
+
+    /// ReerKit: Title of normal state for button; also inspectable from Storyboard.
+    var titleForNormal: String? {
+        get { return base.title(for: .normal) }
+        set { base.setTitle(newValue, for: .normal) }
+    }
+
+    /// ReerKit: Title of selected state for button; also inspectable from Storyboard.
+    var titleForSelected: String? {
+        get { return base.title(for: .selected) }
+        set { base.setTitle(newValue, for: .selected) }
+    }
+    
+    /// ReerKit: Title of focused state for button; also inspectable from Storyboard.
+    var titleForFocused: String? {
+        get { return base.title(for: .focused) }
+        set { base.setTitle(newValue, for: .focused) }
+    }
+    
+    /// ReerKit: Attributed title of disabled state for button.
+    var attributedTitleForDisabled: NSAttributedString? {
+        get { return base.attributedTitle(for: .disabled) }
+        set { base.setAttributedTitle(newValue, for: .disabled) }
+    }
+
+    /// ReerKit: Attributed title of highlighted state for button.
+    var attributedTitleForHighlighted: NSAttributedString? {
+        get { return base.attributedTitle(for: .highlighted) }
+        set { base.setAttributedTitle(newValue, for: .highlighted) }
+    }
+
+    /// ReerKit: Attributed title of normal state for button.
+    var attributedTitleForNormal: NSAttributedString? {
+        get { return base.attributedTitle(for: .normal) }
+        set { base.setAttributedTitle(newValue, for: .normal) }
+    }
+
+    /// ReerKit: Attributed title of selected state for button.
+    var attributedTitleForSelected: NSAttributedString? {
+        get { return base.attributedTitle(for: .selected) }
+        set { base.setAttributedTitle(newValue, for: .selected) }
+    }
+    
+    /// ReerKit: Attributed title of focused state for button.
+    var attributedTitleForFocused: NSAttributedString? {
+        get { return base.attributedTitle(for: .focused) }
+        set { base.setAttributedTitle(newValue, for: .focused) }
+    }
+    
+    private var states: [UIControl.State] {
+        [.normal, .selected, .highlighted, .disabled, .focused]
+    }
+
+    /// ReerKit: Set image for all states.
+    ///
+    /// - Parameter image: UIImage.
+    func setImageForAllStates(_ image: UIImage) {
+        states.forEach { base.setImage(image, for: $0) }
+    }
+
+    /// ReerKit: Set title color for all states.
+    ///
+    /// - Parameter color: UIColor.
+    func setTitleColorForAllStates(_ color: UIColor) {
+        states.forEach { base.setTitleColor(color, for: $0) }
+    }
+
+    /// ReerKit: Set title for all states.
+    ///
+    /// - Parameter title: title string.
+    func setTitleForAllStates(_ title: String) {
+        states.forEach { base.setTitle(title, for: $0) }
+    }
+
+    /// ReerKit: Set attributed title for all states.
+    ///
+    /// - Parameter title: title string.
+    func setAttributedTitleForAllStates(_ title: NSAttributedString) {
+        states.forEach { base.setAttributedTitle(title, for: $0) }
+    }
+    
+    /// ReerKit: Center align title text and image.
+    /// - Parameters:
+    ///   - imageAboveText: set true to make image above title text, default is false, image on left of text.
+    ///   - spacing: spacing between title text and image.
+    func centerTextAndImage(imageAboveText: Bool = false, spacing: CGFloat) {
+        if imageAboveText {
+            // https://stackoverflow.com/questions/2451223/#7199529
+            guard let imageSize = base.imageView?.image?.size,
+                  let text = base.titleLabel?.text,
+                  let font = base.titleLabel?.font
+            else { return }
+
+            let titleSize = text.size(withAttributes: [.font: font])
+
+            let titleOffset = -(imageSize.height + spacing)
+            base.titleEdgeInsets = UIEdgeInsets(top: 0.0, left: -imageSize.width, bottom: titleOffset, right: 0.0)
+
+            let imageOffset = -(titleSize.height + spacing)
+            base.imageEdgeInsets = UIEdgeInsets(top: imageOffset, left: 0.0, bottom: 0.0, right: -titleSize.width)
+
+            let edgeOffset = Swift.abs(titleSize.height - imageSize.height) / 2.0
+            base.contentEdgeInsets = UIEdgeInsets(top: edgeOffset, left: 0.0, bottom: edgeOffset, right: 0.0)
+        } else {
+            let insetAmount = spacing / 2
+            base.imageEdgeInsets = UIEdgeInsets(top: 0, left: -insetAmount, bottom: 0, right: insetAmount)
+            base.titleEdgeInsets = UIEdgeInsets(top: 0, left: insetAmount, bottom: 0, right: -insetAmount)
+            base.contentEdgeInsets = UIEdgeInsets(top: 0, left: insetAmount, bottom: 0, right: insetAmount)
+        }
+    }
+    
+    /// ReerKit: Set background color for specified state.
+    /// - Parameters:
+    ///   - color: The color of the image that will be set as background for the button in the given state.
+    ///   - forState: set the UIControl.State for the desired color.
+    func setBackgroundColor(color: UIColor, forState: UIControl.State) {
+        base.clipsToBounds = true
+        
+        if #available(iOS 10.0, tvOS 10.0, *) {
+            let colorImage = UIGraphicsImageRenderer(size: CGSize(width: 1, height: 1)).image { context in
+                color.setFill()
+                context.fill(CGRect(x: 0, y: 0, width: 1, height: 1))
+                base.draw(.zero)
+            }
+            base.setBackgroundImage(colorImage, for: forState)
+            return
+        }
+        
+        UIGraphicsBeginImageContext(CGSize(width: 1, height: 1))
+        if let context = UIGraphicsGetCurrentContext() {
+            context.setFillColor(color.cgColor)
+            context.fill(CGRect(x: 0, y: 0, width: 1, height: 1))
+            let colorImage = UIGraphicsGetImageFromCurrentImageContext()
+            UIGraphicsEndImageContext()
+            base.setBackgroundImage(colorImage, for: forState)
+        }
+    }
+}
+
+#endif
---
+++ Tests/UIKitTests/UIButtonExtensionsTests.swift
@@ -0,0 +1,283 @@
+//
+//  UIButtonExtensionsTests.swift
+//  ReerKit-iOSTests
+//
+//  Created by phoenix on 2023/4/19.
+//  Copyright © 2023 reers. All rights reserved.
+//
+
+@testable import ReerKit
+import XCTest
+
+#if canImport(UIKit) && !os(watchOS)
+import UIKit
+
+final class UIButtonExtensionsTests: XCTestCase {
+    func testImageForDisabled() {
+        let button = UIButton()
+        XCTAssertEqual(button.re.imageForDisabled, button.image(for: .disabled))
+
+        let newImage = UIImage()
+        button.re.imageForDisabled = newImage
+        XCTAssertEqual(button.re.imageForDisabled, newImage)
+    }
+
+    func testImageForHighlighted() {
+        let button = UIButton()
+        XCTAssertEqual(button.re.imageForHighlighted, button.image(for: .highlighted))
+
+        let newImage = UIImage()
+        button.re.imageForHighlighted = newImage
+        XCTAssertEqual(button.re.imageForHighlighted, newImage)
+    }
+
+    func testImageForNormal() {
+        let button = UIButton()
+        XCTAssertEqual(button.re.imageForNormal, button.image(for: .normal))
+
+        let newImage = UIImage()
+        button.re.imageForNormal = newImage
+        XCTAssertEqual(button.re.imageForNormal, newImage)
+    }
+
+    func testImageForSelected() {
+        let button = UIButton()
+        XCTAssertEqual(button.re.imageForSelected, button.image(for: .selected))
+
+        let newImage = UIImage()
+        button.re.imageForSelected = newImage
+        XCTAssertEqual(button.re.imageForSelected, newImage)
+    }
+
+    func testImageForFocused() {
+        let button = UIButton()
+        XCTAssertEqual(button.re.imageForFocused, button.image(for: .focused))
+
+        let newImage = UIImage()
+        button.re.imageForFocused = newImage
+        XCTAssertEqual(button.re.imageForFocused, newImage)
+    }
+    
+    func testTitleColorForDisabled() {
+        let button = UIButton()
+        XCTAssertEqual(button.re.titleColorForDisabled, button.titleColor(for: .disabled))
+
+        button.re.titleColorForDisabled = .green
+        XCTAssertEqual(button.re.titleColorForDisabled, .green)
+    }
+
+    func testTitleColorForHighlighted() {
+        let button = UIButton()
+        XCTAssertEqual(button.re.titleColorForHighlighted, button.titleColor(for: .highlighted))
+
+        button.re.titleColorForHighlighted = .green
+        XCTAssertEqual(button.re.titleColorForHighlighted, .green)
+    }
+
+    func testTitleColorForNormal() {
+        let button = UIButton()
+        XCTAssertEqual(button.re.titleColorForNormal, button.titleColor(for: .normal))
+
+        button.re.titleColorForNormal = .green
+        XCTAssertEqual(button.re.titleColorForNormal, .green)
+    }
+
+    func testTitleColorForSelected() {
+        let button = UIButton()
+        XCTAssertEqual(button.re.titleColorForSelected, button.titleColor(for: .selected))
+
+        button.re.titleColorForSelected = .green
+        XCTAssertEqual(button.re.titleColorForSelected, .green)
+    }
+
+    func testTitleColorForFocused() {
+        let button = UIButton()
+        XCTAssertEqual(button.re.titleColorForFocused, button.titleColor(for: .focused))
+
+        button.re.titleColorForFocused = .green
+        XCTAssertEqual(button.re.titleColorForFocused, .green)
+    }
+    
+    func testTitleForDisabled() {
+        let button = UIButton()
+        XCTAssertEqual(button.re.titleForDisabled, button.title(for: .disabled))
+
+        let title = "Disabled"
+        button.re.titleForDisabled = title
+        XCTAssertEqual(button.re.titleForDisabled, title)
+    }
+
+    func testTitleForHighlighted() {
+        let button = UIButton()
+        XCTAssertEqual(button.re.titleForHighlighted, button.title(for: .highlighted))
+
+        let title = "Highlighted"
+        button.re.titleForHighlighted = title
+        XCTAssertEqual(button.re.titleForHighlighted, title)
+    }
+
+    func testTitleForNormal() {
+        let button = UIButton()
+        XCTAssertEqual(button.re.titleForNormal, button.title(for: .normal))
+
+        let title = "Normal"
+        button.re.titleForNormal = title
+        XCTAssertEqual(button.re.titleForNormal, title)
+    }
+
+    func testTitleForSelected() {
+        let button = UIButton()
+        XCTAssertEqual(button.re.titleForSelected, button.title(for: .selected))
+
+        let title = "Selected"
+        button.re.titleForSelected = title
+        XCTAssertEqual(button.re.titleForSelected, title)
+    }
+    
+    func testTitleForFocused() {
+        let button = UIButton()
+        XCTAssertEqual(button.re.titleForFocused, button.title(for: .focused))
+
+        let title = "Focused"
+        button.re.titleForFocused = title
+        XCTAssertEqual(button.re.titleForFocused, title)
+    }
+    
+    func testAttributedTitleForDisabled() {
+        let button = UIButton()
+        XCTAssertEqual(button.re.attributedTitleForDisabled, button.attributedTitle(for: .disabled))
+
+        let title = NSAttributedString(string: "Disabled", attributes: [.foregroundColor:UIColor.yellow, .backgroundColor:UIColor.green])
+        button.re.attributedTitleForDisabled = title
+        XCTAssertEqual(button.re.attributedTitleForDisabled, title)
+    }
+
+    func testAttributedTitleForHighlighted() {
+        let button = UIButton()
+        XCTAssertEqual(button.re.attributedTitleForHighlighted, button.attributedTitle(for: .highlighted))
+
+        let title = NSAttributedString(string: "Highlighted", attributes: [.foregroundColor:UIColor.yellow, .backgroundColor:UIColor.green])
+        button.re.attributedTitleForHighlighted = title
+        XCTAssertEqual(button.re.attributedTitleForHighlighted, title)
+    }
+
+    func testAttributedTitleForNormal() {
+        let button = UIButton()
+        XCTAssertEqual(button.re.attributedTitleForNormal, button.attributedTitle(for: .normal))
+
+        let title = NSAttributedString(string: "Normal", attributes: [.foregroundColor:UIColor.yellow, .backgroundColor:UIColor.green])
+        button.re.attributedTitleForNormal = title
+        XCTAssertEqual(button.re.attributedTitleForNormal, title)
+    }
+
+    func testAttributedTitleForSelected() {
+        let button = UIButton()
+        XCTAssertEqual(button.re.attributedTitleForSelected, button.attributedTitle(for: .selected))
+
+        let title = NSAttributedString(string: "Selected", attributes: [.foregroundColor:UIColor.yellow, .backgroundColor:UIColor.green])
+        button.re.attributedTitleForSelected = title
+        XCTAssertEqual(button.re.attributedTitleForSelected, title)
+    }
+
+    func testAttributedTitleForFocused() {
+        let button = UIButton()
+        XCTAssertEqual(button.re.attributedTitleForFocused, button.attributedTitle(for: .focused))
+
+        let title = NSAttributedString(string: "Focused", attributes: [.foregroundColor:UIColor.yellow, .backgroundColor:UIColor.green])
+        button.re.attributedTitleForFocused = title
+        XCTAssertEqual(button.re.attributedTitleForFocused, title)
+    }
+    
+    func testSetImageForAllStates() {
+        let button = UIButton()
+        let image = UIImage()
+        button.re.setImageForAllStates(image)
+        
+        XCTAssertEqual(button.re.imageForDisabled, image)
+        XCTAssertEqual(button.re.imageForHighlighted, image)
+        XCTAssertEqual(button.re.imageForNormal, image)
+        XCTAssertEqual(button.re.imageForSelected, image)
+        XCTAssertEqual(button.re.imageForFocused, image)
+    }
+
+    func testSetTitleColorForAllStates() {
+        let button = UIButton()
+        let color = UIColor.green
+        button.re.setTitleColorForAllStates(color)
+        
+        XCTAssertEqual(button.re.titleColorForDisabled, color)
+        XCTAssertEqual(button.re.titleColorForHighlighted, color)
+        XCTAssertEqual(button.re.titleColorForNormal, color)
+        XCTAssertEqual(button.re.titleColorForSelected, color)
+        XCTAssertEqual(button.re.titleColorForFocused, color)
+    }
+
+    func testSetTitleForAllStates() {
+        let button = UIButton()
+        let title = "Title"
+        button.re.setTitleForAllStates(title)
+        
+        XCTAssertEqual(button.re.titleForDisabled, title)
+        XCTAssertEqual(button.re.titleForHighlighted, title)
+        XCTAssertEqual(button.re.titleForNormal, title)
+        XCTAssertEqual(button.re.titleForSelected, title)
+        XCTAssertEqual(button.re.titleForFocused, title)
+    }
+
+    func testCenterTextAndImage() {
+//        let button = UIButton(frame: CGRect(x: 10, y: 10, width: 100, height: 100))
+//        let image = UIImage(color: .green, size: CGSize(width: 10, height: 10))
+//        button.re.setTitleForAllStates("Title")
+//        button.re.setImageForAllStates(image)
+//
+//        XCTAssertNotNil(button.imageView)
+//        XCTAssertNotNil(button.titleLabel)
+//
+//        let spacing: CGFloat = 20
+//
+//        // Image on the left of text
+//        button.re.centerTextAndImage(spacing: spacing)
+//        var imageFrame = button.imageView!.frame
+//        var titleFrame = button.titleLabel!.frame
+//
+//        XCTAssert(titleFrame.midX > imageFrame.midX)
+//        XCTAssertEqual(titleFrame.midY, imageFrame.midY, accuracy: 1.0)
+//        XCTAssertEqual(titleFrame.minX - spacing, imageFrame.maxX, accuracy: 1.0)
+//
+//        // Image above text
+//        button.re.centerTextAndImage(imageAboveText: true, spacing: spacing)
+//        imageFrame = button.imageView!.frame
+//        titleFrame = button.titleLabel!.frame
+//
+//        XCTAssert(titleFrame.midY > imageFrame.midY)
+//        XCTAssertEqual(titleFrame.midX, imageFrame.midX, accuracy: 1.5)
+//        XCTAssertEqual(titleFrame.minY - spacing, imageFrame.maxY, accuracy: 1.0)
+    }
+    
+    func testSetAttrbiutedTitleForAllStates() {
+        let button = UIButton()
+        let title = NSAttributedString(string: "Title", attributes: [.foregroundColor:UIColor.yellow, .backgroundColor:UIColor.green])
+        button.re.setAttributedTitleForAllStates(title)
+
+        XCTAssertEqual(button.re.attributedTitleForDisabled, title)
+        XCTAssertEqual(button.re.attributedTitleForHighlighted, title)
+        XCTAssertEqual(button.re.attributedTitleForNormal, title)
+        XCTAssertEqual(button.re.attributedTitleForSelected, title)
+        XCTAssertEqual(button.re.attributedTitleForFocused, title)
+    }
+    
+    func testSetBackgroundColorForState() {
+//        let button = UIButton()
+//        let color = UIColor.orange
+//
+//        button.re.setBackgroundColor(color: color, forState: .highlighted)
+//
+//        let highlightedBackgroundImage = button.backgroundImage(for: .highlighted)
+//        let averageColor = highlightedBackgroundImage!.averageColor()!
+//
+//        XCTAssertEqual(averageColor, color, accuracy: 0.01)
+    }
+}
+
+#endif
+

cdf18b0d923b7d2a14ce451379a309695bc45182
Phoenix <x.rhythm@qq.com>
2023-06-08 02:23:48 +0800

    feat: add iOS example project.

---
+++ Example/iOS_Example/iOS_Example/ExampleListViewController.swift
@@ -0,0 +1,88 @@
+//
+//  ExampleListViewController.swift
+//  ReerKit
+//
+//  Created by YuYue on 2022/7/25.
+//
+
+import UIKit
+
+struct Example {
+    var title: String
+    var vcName: String
+}
+
+class ExampleListViewController: UIViewController {
+    
+    
+    let demos = [
+        Example(title: "KeyboardManager", vcName: "KeyboardManagerDemoViewController"),
+    ]
+    
+    
+    // MARK: UI Properties
+    
+    let tableView = UITableView()
+    
+    init() {
+        super.init(nibName: nil, bundle: nil)
+        self.title = "Examples"
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    
+    // MARK: View Life Cycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.addSubview(self.tableView)
+        self.tableView.dataSource = self
+        self.tableView.delegate = self
+        self.tableView.register(UITableViewCell.self, forCellReuseIdentifier: "user")
+    }
+    
+    
+    // MARK: Layout
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        self.tableView.frame = self.view.bounds
+    }
+    
+}
+
+
+// MARK: - UITableViewDataSource
+
+extension ExampleListViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return demos.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "user", for: indexPath)
+        let demo = demos[indexPath.row]
+        cell.textLabel?.text = demo.title
+        cell.detailTextLabel?.textColor = .gray
+        cell.accessoryType = .disclosureIndicator
+        return cell
+    }
+}
+
+
+// MARK: - UITableViewDelegate
+
+extension ExampleListViewController: UITableViewDelegate {
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath : IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: false)
+        let demo = demos[indexPath.row]
+        guard let clazz = NSClassFromString(demo.vcName) as? UIViewController.Type else { return }
+        let viewController = clazz.init()
+        navigationController?.pushViewController(viewController, animated: true)
+    }
+}
+
---
+++ Example/iOS_Example/iOS_Example/KeyboardManagerDemoViewController.swift
@@ -0,0 +1,79 @@
+//
+//  KeyboardManagerDemoViewController.swift
+//  iOS_Example
+//
+//  Created by phoenix on 2023/6/8.
+//
+
+import UIKit
+import ReerKit
+
+@objc(KeyboardManagerDemoViewController)
+class KeyboardManagerDemoViewController: UIViewController {
+    
+    deinit {
+        print(#function)
+    }
+    
+    var defaultFrame: CGRect = .zero
+    var textField: UITextField!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+        
+        KeyboardManager.shared.addObserver(self)
+        
+        let button = UIButton()
+        button.re.size = .re(80, 40)
+        button.re.centerX = view.re.centerX
+        button.re.centerY = view.re.height / 3.0
+        button.setTitle("Toggle", for: .normal)
+        button.setTitleColor(.red, for: .normal)
+        button.addTarget(self, action: #selector(toggle), for: .touchUpInside)
+        view.addSubview(button)
+        
+        textField = UITextField()
+        textField.placeholder = "Please input text"
+        textField.backgroundColor = .lightGray
+        textField.re.size = .re(200, 30)
+        textField.re.centerX = view.re.centerX
+        textField.re.bottom = view.re.height - (UIApplication.shared.re.keyWindow?.safeAreaInsets.bottom ?? 0)
+        defaultFrame = textField.frame
+        view.addSubview(textField)
+        textField.becomeFirstResponder()
+    }
+    
+    @objc
+    func toggle() {
+        if textField.isFirstResponder {
+            textField.resignFirstResponder()
+        } else {
+            textField.becomeFirstResponder()
+        }
+    }
+}
+
+extension KeyboardManagerDemoViewController: KeyboardObserver {
+    
+    func keyboardChanged(with transition: KeyboardTransition) {
+        print(#function)
+        if transition.toVisible {
+            print(KeyboardManager.shared.keyboardView)
+        }
+        UIView.animate(withDuration: transition.animationDuration, delay: 0, options: transition.animationOption) {
+            let kbFrame = KeyboardManager.shared.convertRect(transition.toFrame, toView: self.view)
+            if transition.toVisible {
+                var textFrame = self.textField.frame
+                textFrame.origin.x = 0
+                textFrame.size.width = kbFrame.size.width
+                textFrame.origin.y = kbFrame.origin.y - textFrame.size.height
+                self.textField.frame = textFrame
+            } else {
+                self.textField.frame = self.defaultFrame
+            }
+        } completion: { _ in
+            
+        }
+    }
+}

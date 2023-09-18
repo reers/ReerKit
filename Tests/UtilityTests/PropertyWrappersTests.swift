3b230d00669588b37ad5948ff4e95ed95cbddb2e
Phoenix <x.rhythm@qq.com>
2023-09-18 16:17:36 +0800

    feat: add @Clamped property wrapper

---
+++ Tests/UtilityTests/PropertyWrappersTests.swift
@@ -0,0 +1,26 @@
+//
+//  PropertyWrappersTests.swift
+//  ReerKit-iOSTests
+//
+//  Created by phoenix on 2023/9/18.
+//  Copyright © 2023 reers. All rights reserved.
+//
+
+import XCTest
+@testable import ReerKit
+
+final class PropertyWrappersTests: XCTestCase {
+    
+    @Clamped(0...150)
+    var age: Int = 0
+
+    func testClamped() {
+        age = -4
+        XCTAssertEqual(age, 0)
+        age = 200
+        XCTAssertEqual(age, 150)
+        age = 80
+        XCTAssertEqual(age, 80)
+    }
+
+}

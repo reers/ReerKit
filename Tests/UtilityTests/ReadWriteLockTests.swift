bdea45a6c9538243d7a9a068d04c35fcba46dd89
Phoenix <x.rhythm@qq.com>
2023-11-28 01:08:35 +0800

    test: update ut for ReadWriteLock

--- Tests/UtilityTests/ReadWriteLockTests.swift
+++ Tests/UtilityTests/ReadWriteLockTests.swift
@@ -10,56 +10,39 @@ import XCTest
 @testable import ReerKit
 
 class ReadWriteLockTests: XCTestCase {
-
-    func testReadWriteLock() {
-        var result = ""
-        let rwlock = ReadWriteLock()
-        DispatchQueue.global().async {
-            
-            rwlock.writeLock()
-            result += "1"
-            for _ in 0...10000 {
-                // do something
-            }
-            result += "2"
-            rwlock.writeUnlock()
-        }
-        for i in 0...10 {
-            DispatchQueue.global().async {
-                rwlock.readLock()
-                result += "3"
-                for _ in 0..<i {}
-                rwlock.readUnlock()
-                if i == 0 {
-                    XCTAssertTrue(result.hasPrefix("123"))
-                }
-            }
-        }
-    }
     
-    func testReadWriteLockClosure() {
-        var result = ""
-        let rwlock = ReadWriteLock()
+    @RWLocked
+    var array: [Int] = []
+    
+    func testRWLocked() {
+        let expectation = expectation(description: #function)
         DispatchQueue.global().async {
-            rwlock.writeAround {
-                result += "1"
-                for _ in 0...10000 {
-                    // do something
-                }
-                result += "2"
+            print("write group 1")
+            self.$array.write {
+                $0.append(1)
+                print("append 1")
+                sleep(1)
+                $0.append(2)
+                print("append 2")
             }
         }
-        for i in 0...10 {
-            DispatchQueue.global().async {
-                rwlock.readAround {
-                    result += "3"
-                    for _ in 0..<i {}
-                }
-                if i == 0 {
-                    XCTAssertTrue(result.hasPrefix("123"))
-                }
+        delay(0.5, onQueue: .global()) {
+            print("write gourp 2")
+            self.$array.write {
+                $0.append(3)
+                print("append 3")
+            }
+            XCTAssertEqual(self.array.re[0..<3], [1, 2, 3])
+            expectation.fulfill()
+        }
+        delay(0.2, onQueue: .global()) {
+            print("read group")
+            self.$array.read {
+                XCTAssertEqual($0, [1, 2])
+                print("read")
             }
         }
+        
+        waitForExpectations(timeout: 10)
     }
-
 }

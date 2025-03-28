0d130c9123188f15e191e1137f2d2a7c198a9c7d
Phoenix <x.rhythm@qq.com>
2025-03-28 20:12:45 +0800

    feat: add font extensions

--- Sources/ReerKit/Shared/Font+REExtensions.swift
+++ Sources/ReerKit/Shared/Font+REExtensions.swift
@@ -31,3 +31,63 @@ import AppKit
 /// ReerKit: Font
 public typealias REFont = NSFont
 #endif
+
+public extension REFont {
+    /// Ultra Light weight (W1)
+    /// Weight value: 100
+    static func re(ultraLight fontSize: CGFloat) -> REFont {
+        return REFont.systemFont(ofSize: fontSize, weight: .ultraLight)
+    }
+    
+    /// Thin weight (W2)
+    /// Weight value: 200
+    static func re(thin fontSize: CGFloat) -> REFont {
+        return REFont.systemFont(ofSize: fontSize, weight: .thin)
+    }
+    
+    /// Light weight (W3)
+    /// Weight value: 300
+    static func re(light fontSize: CGFloat) -> REFont {
+        return REFont.systemFont(ofSize: fontSize, weight: .light)
+    }
+    
+    /// Regular weight (W4)
+    /// Weight value: 400
+    /// This is the default system font weight
+    static func re(regular fontSize: CGFloat) -> REFont {
+        return REFont.systemFont(ofSize: fontSize, weight: .regular)
+    }
+    
+    /// Medium weight (W5)
+    /// Weight value: 500
+    static func re(medium fontSize: CGFloat) -> REFont {
+        return REFont.systemFont(ofSize: fontSize, weight: .medium)
+    }
+    
+    /// Semibold weight (W6)
+    /// Weight value: 600
+    /// Also known as Demibold
+    static func re(semibold fontSize: CGFloat) -> REFont {
+        return REFont.systemFont(ofSize: fontSize, weight: .semibold)
+    }
+    
+    /// Bold weight (W7)
+    /// Weight value: 700
+    static func re(bold fontSize: CGFloat) -> REFont {
+        return REFont.systemFont(ofSize: fontSize, weight: .bold)
+    }
+    
+    /// Heavy weight (W8)
+    /// Weight value: 800
+    /// Also known as Extra Bold
+    static func re(heavy fontSize: CGFloat) -> REFont {
+        return REFont.systemFont(ofSize: fontSize, weight: .heavy)
+    }
+    
+    /// Black weight (W9)
+    /// Weight value: 900
+    /// The heaviest weight
+    static func re(black fontSize: CGFloat) -> REFont {
+        return REFont.systemFont(ofSize: fontSize, weight: .black)
+    }
+}

1fdf5fe9d208f6c84314425a0b4d7c0616801816
Phoenix <x.rhythm@qq.com>
2025-12-05 13:37:54 +0800

    Update example for SwiftUI.Color

--- Example/macOS_Example/macOS_Example/ContentView.swift
+++ Example/macOS_Example/macOS_Example/ContentView.swift
@@ -10,13 +10,16 @@ import ReerKit
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("SGVsbG8sIHdvcmxkIQ==".re.base64Decoded!)
+        ZStack {
+            Color.re("FD7979").re.darken(colorSpace: .displayP3)
+            VStack {
+                Image(systemName: "globe")
+                    .imageScale(.large)
+                    .foregroundStyle(.white)
+                Text("SGVsbG8sIHdvcmxkIQ==".re.base64Decoded!)
+            }
+            .padding()
         }
-        .padding()
     }
 }
 
